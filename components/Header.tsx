4040358fb9187bcc90278cd8015969911ebf82e9
MAmmarShaikh01 <shaikhurwa61@gmail.com>
2024-11-10 10:31:53 +0500

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,16 @@
 import Link from 'next/link';
-import styles from '../CSS/Header.module.css';
 
 const Header = () => {
   return (
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <h1 className={styles.logo}>
+    <header className="bg-gray-800 text-gray-100 py-4 shadow-md sticky top-0 z-10">
+      <div className="max-w-7xl mx-auto px-4 sm:px-8 flex justify-between items-center">
+        <h1 className="md:text-2xl font-bold text-blue-600">
           <Link href="/">My FancyWebsite</Link>
         </h1>
-        <nav className={styles.nav}>
-          <Link href="/" className={styles.navLink}>Home</Link>
-          <Link href="/about" className={styles.navLink}>About</Link>
-          <Link href="/contact" className={styles.navLink}>Contact</Link>
+        <nav className=" flex gap-6 text-sm md:text-lg font-bold px-2">
+          <Link href="/" className="text-gray-100 font-medium transition-colors duration-300 hover:text-blue-600">Home</Link>
+          <Link href="/about" className="text-gray-100 font-medium transition-colors duration-300 hover:text-blue-600">About</Link>
+          <Link href="/contact" className="text-gray-100 font-medium transition-colors duration-300 hover:text-blue-600">Contact</Link>
         </nav>
       </div>
     </header>
